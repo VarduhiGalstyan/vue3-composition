1036a0f568b61f96bdf2d04f73ca898ef68d70da
Varduhi <varduhigalstyann71@gmail.com>
2024-10-07 15:51:08 +0400

    end 8

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,31 +1,33 @@
 <template>
     <div class="text-center">
+        <!-- <div>{{ counterData.title }}</div> -->
         <div>{{ counterTitle }}</div>
-
         <div>
             <button @click.prevent="decreaseCounter()">-</button>
-            <span>{{ counter }}</span>
+            <span>{{ counterData.count }}</span>
             <button @click.prevent="increaseCounter()">+</button>
             <div>
-                <input type="text" v-model="counterTitle"/>
+                <input type="text" v-model="counterTitle" />
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
-    import {ref} from 'vue';
+    import {reactive, ref} from 'vue';
+
+    const counterTitle = 'Counter Title'
+    const counterData = reactive({
+        count: 0,
+        // title: 'Counter Title from reactive'
+    });
 
     
-
-    const counter = ref(10);
-    const counterTitle = ref('Counter Title');
-
     const increaseCounter = () => {
-        counter.value++;
+        counterData.count++;
     }
     const decreaseCounter = () => {
-        counter.value--;
+        counterData.count--;
     }
 </script>
 
