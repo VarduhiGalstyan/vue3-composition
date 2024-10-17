ce665de9539dcdb524e2c45dfc9cdb48400c4d38
Varduhi <varduhigalstyann71@gmail.com>
2024-10-17 13:52:18 +0400

    47. JavaScript Hooks for the Vue Transitions in the Vue 3 composition API - Vue 3

--- src/components/TransitionComponent.vue
+++ src/components/TransitionComponent.vue
@@ -3,7 +3,15 @@
         <button @click="show = !show" >Toggle</button>
 
         <!-- <Transition > -->
-        <Transition name="bounce" enter-active-class="bounce-active" leave-active-class="leave-active">
+        <Transition
+        @before-enter="onBeforeEnter"
+        @enter="onEnter"
+        @after-enter="onAfterEnter"
+
+        @before-leave="onBeforeLeave"
+        @leave="onLeave"
+        @after-leave="onAfterLeave"
+        >
             <div v-if="show" style="text-align: center">Leela Web Dev</div>
         </Transition>
     </div>
@@ -13,6 +21,34 @@
 import { ref } from 'vue';
 
 const show = ref(true);
+
+const onBeforeEnter =(el) => {
+    console.log("On Before enter", el);
+}
+const onEnter = (el, done) => {
+    console.log("On enter", el);
+    const animate = el.animate([{transform: 'scale(0)' }, {}], {duration: 1000});
+    animate.onfinish = () => {
+        done();
+    };
+}
+const onAfterEnter = (el) => {
+    console.log("On After enter", el);
+}
+
+const onBeforeLeave =(el) => {
+    console.log("On Before Leave", el);
+}
+const onLeave = (el, done) => {
+    console.log('On Leave', el);
+    const animate = el.animate([{}, {transform: 'scale(0)' }], {duration: 1000});
+    animate.onfinish = () => {
+        done();
+    };
+}
+const onAfterLeave = (el) => {
+    console.log("On After Leave", el);
+}
 </script>
 
 <style scoped>
