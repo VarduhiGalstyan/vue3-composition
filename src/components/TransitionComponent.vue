b943b2069bcf6285c81599e833a4d34a962884d0
Varduhi <varduhigalstyann71@gmail.com>
2024-10-17 12:04:44 +0400

    45. CSS Transition classes applied for applying animation in Vue 3 composition API - Vue 3

--- src/components/TransitionComponent.vue
+++ src/components/TransitionComponent.vue
@@ -2,7 +2,8 @@
     <div>
         <button @click="show = !show" >Toggle</button>
 
-        <Transition>
+        <!-- <Transition > -->
+        <Transition name="fade">
             <div v-if="show">Leela Web Dev</div>
         </Transition>
     </div>
@@ -14,11 +15,32 @@ import { ref } from 'vue';
 const show = ref(true);
 </script>
 <style scoped>
-.v-enter-from, .v-leave-to
+/* .v-enter-from, .v-leave-to
 {
 opacity: 0;
 }
 .v-enter-active, .v-leave-to{
     transition: opacity 2s ease;
-}
+} */
+
+ /* .v-enter-from, .v-leave-to{
+    opacity: 0;
+ }
+ .v-enter-to, .v-leave-from{
+    opacity: 1;
+ }
+ .v-enter-active, .v-leave-active {
+    transition: opacity 2s ease;
+ } */
+
+ .fade-enter-from, .fade-leave-to{
+    opacity: 0;
+ }
+ .fade-enter-to, .fade-leave-from{
+    opacity: 1;
+ }
+ .fade-enter-active, .fade-leave-active {
+    transition: opacity 2s ease;
+ }
+
 </style>
