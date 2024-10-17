24a7730fa63d7adb53908c87486287f57b807679
Varduhi <varduhigalstyann71@gmail.com>
2024-10-17 14:18:18 +0400

    48kisat

---
+++ src/components/CompB.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>Comp B</div>
+</template>
+<script setup>
+</script>
--- src/components/TransitionComponent.vue
+++ src/components/TransitionComponent.vue
@@ -1,80 +1,27 @@
 <template>
-    <div>
-        <button @click="show = !show" >Toggle</button>
-
-        <!-- <Transition > -->
-        <Transition
-        @before-enter="onBeforeEnter"
-        @enter="onEnter"
-        @after-enter="onAfterEnter"
-
-        @before-leave="onBeforeLeave"
-        @leave="onLeave"
-        @after-leave="onAfterLeave"
-        >
-            <div v-if="show" style="text-align: center">Leela Web Dev</div>
-        </Transition>
-    </div>
+    <Transition mode="in-out">
+        <button v-if="docState == 'edit'" @click="docState = 'save'">Edit</button>
+        <button v-else-if="docState == 'save'"  @click="docState = 'cancel'">Save</button>
+        <button v-else="docState == 'cancel'"  @click="docState = 'edit'">Cancel</button>
+    </Transition>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
 const show = ref(true);
+const docState = ref('edit')
 
-const onBeforeEnter =(el) => {
-    console.log("On Before enter", el);
-}
-const onEnter = (el, done) => {
-    console.log("On enter", el);
-    const animate = el.animate([{transform: 'scale(0)' }, {}], {duration: 1000});
-    animate.onfinish = () => {
-        done();
-    };
-}
-const onAfterEnter = (el) => {
-    console.log("On After enter", el);
-}
-
-const onBeforeLeave =(el) => {
-    console.log("On Before Leave", el);
-}
-const onLeave = (el, done) => {
-    console.log('On Leave', el);
-    const animate = el.animate([{}, {transform: 'scale(0)' }], {duration: 1000});
-    animate.onfinish = () => {
-        done();
-    };
-}
-const onAfterLeave = (el) => {
-    console.log("On After Leave", el);
-}
 </script>
 
 <style scoped>
-/* .bounce-enter-active {
-    animation: bounce-in 0.5s;
+.v-enter-from{
+    transform: translateY(-30px);
 }
-
-.bounce-leave-active{
-    animation: bounce-in 0.5s reverse; 
-} */
-.bounce-active {
-    animation: bounce-in 0.5s;
+.v-leave-to{
+    transform: translateY(30px);
 }
-
-.leave-active{
-    animation: bounce-in 0.5s reverse; 
-}
-@keyframes bounce-in{
-    0%{
-        transform: scale(0);
-    }
-    50%{
-        transform: scale(1.25);
-    }
-    100%{
-        transform: scale(1);
-    }
+.v-enter-active, .v-leave-active{
+    transform: all 1s;
 }
 </style>
