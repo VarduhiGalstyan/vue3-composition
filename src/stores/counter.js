5ad8a7bac6f0269faa1de66e1f73125f89649274
Varduhi <varduhigalstyann71@gmail.com>
2024-10-21 17:47:09 +0400

    67. Defining a Store in Pinia using the Options Api in the Vue3 Composition API - Vue 3

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,14 +1,31 @@
-import { defineStore } from "pinia";
+// import { defineStore } from "pinia";
 
-export const useCounterStore = defineStore('counter', {
-    state(){
+// export const useCounterStore = defineStore('counter', {
+//     state(){
+//         return{
+//             count: 0
+//         };
+//     },
+//     actions: {
+//         increment() {
+//             this.count++;
+//         }
+//     }
+// });
+import { defineStore } from 'pinia';
+
+export const useCounterStore = defineStore ('counterStore', {
+    state: () => {
         return{
             count: 0
         };
     },
+    getters: {
+        doubleCount: (state) => state.count * 2
+    },
     actions: {
         increment() {
-            this.count++;
+            this.count ++;
         }
     }
 });
