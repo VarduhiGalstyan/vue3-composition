f2c11e08c3fa929e8fce2716d96d1f781717ede7
Varduhi <varduhigalstyann71@gmail.com>
2024-10-22 18:09:32 +0400

    74. Create a Sample Vue 3 Composition API Project and installed Vue router - Vue 3

---
+++ noteslist-vue/src/App.vue
@@ -0,0 +1,11 @@
+
+<template>
+  <div>
+    <div>
+      <RouterLink :to="{name: 'notes'}">Notes</RouterLink> |
+      <RouterLink :to="{name: 'stats'}">Notes</RouterLink>
+
+    </div>
+    <RouterView/>
+  </div>
+</template>
