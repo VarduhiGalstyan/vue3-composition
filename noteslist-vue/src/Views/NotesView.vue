ec24e87e5ee351aa34a63b0aa8e2b3aa1c2555a3
Varduhi <varduhigalstyann71@gmail.com>
2024-10-23 15:13:13 +0400

    78. Add New Note into the Notes array using Click Event in NotesList Vue Composition API - Vue 3

--- noteslist-vue/src/Views/NotesView.vue
+++ noteslist-vue/src/Views/NotesView.vue
@@ -1,28 +1,57 @@
 <template>
-<div class="has-background-danger-dark my-4 p-4">
-<div class="field">
-  <label class="label">Note</label>
-  <div class="control">
-    <textarea class="textarea" placeholder="Textarea"></textarea>
-  </div>
-</div>
+  <div class="has-background-danger-dark my-4 p-4">
+    <div class="field">
+      <label class="label">Note</label>
+      <div class="control">
+        <textarea class="textarea" placeholder="Add New Note"
+         v-model="newNote" ref="newNoteRef"></textarea>
+      </div>
+    </div>
 
-<div class="field is-grouped is-grouped-right">
-  <div class="control">
-    <button class="button is-link is-danger">Add New Note</button>
-  </div>
-</div>
-</div>
-
-  <div class="card mb-4" >
-  <div class="card-content">
-    <div class="content">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed rem vitae repellat amet nam dicta ab dolor obcaecati voluptas error vero soluta, assumenda suscipit. Ex doloremque illo quasi eius eaque!
+    <div class="field is-grouped is-grouped-right">
+      <div class="control">
+        <button :disabled="!newNote" class="button is-link is-danger"
+         @click.prevent="addNote">
+         Add New Note
+        </button>
+      </div>
     </div>
   </div>
-  <footer class="card-footer">
-    <a href="#" class="card-footer-item">Edit</a>
-    <a href="#" class="card-footer-item">Delete</a>
-  </footer>
-</div>
+
+  <div class="card mb-4"  v-for="note in notes" :key="note.id">
+    <div class="card-content">
+      <div class="content">
+        {{ note.content }}
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Edit</a>
+      <a href="#" class="card-footer-item">Delete</a>
+    </footer>
+  </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const notes = ref([
+  {id: 'id1', content: 'First Note'},
+  {id: 'id2', content: 'Second Note'}
+]);
+
+const newNoteRef = ref(null);
+
+const newNote = ref('');
+
+const addNote = () => {
+  const currentDate = new Date().getTime().toString();
+  const note = {
+    id: currentDate,
+    content: newNote.value
+  };
+
+  notes.value.unshift(note);
+  newNote.value ='',  
+  newNoteRef.value.focus();
+};
+</script>
