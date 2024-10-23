063f8c59f26b21c754edaa470a580835d15aa2ac
Varduhi <varduhigalstyann71@gmail.com>
2024-10-23 12:51:21 +0400

    76. Making the NavBar mobile Responsive and adding RouterLink | Vue Composition API - Vue 3

--- noteslist-vue/src/components/Layaut/NavBar.vue
+++ noteslist-vue/src/components/Layaut/NavBar.vue
@@ -1,11 +1,19 @@
 <template>
 <nav class="navbar is-danger" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-    <a class="navbar-item is-size-4 is-family-monospace" href="https://bulma.io">
+    <RouterLink :to="{name: 'notes'}" class="navbar-item is-size-4 is-family-monospace" >
       NotesList
-    </a>
+    </RouterLink>
 
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+    <a 
+      role="button" 
+      class="navbar-burger " 
+      :class="{'is-active' : showNavBarMenu}"
+      @click.prevent=" showNavBarMenu = !showNavBarMenu"
+      aria-label="menu" 
+      aria-expanded="false" 
+      data-target="navbarBasicExample"
+    >
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
@@ -13,12 +21,28 @@
     </a>
   </div>
 
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-end">
-      <a class="navbar-item "> Notes </a>
+  <div id="navbarBasicExample" :class="{'is-active' : showNavBarMenu }" class="navbar-menu">
+    <div class="navbar-end" >
+      <RouterLink :to="{name: 'notes'}" active-class="is-active" class="navbar-item "> Notes </RouterLink>
 
-      <a class="navbar-item"> Stats </a>
+      <RouterLink :to="{name: 'stats'}" active-class="is-active" class="navbar-item"> Stats </RouterLink>
     </div>
   </div>
 </nav>
 </template>
+<script setup>
+import { ref } from 'vue';
+
+const showNavBarMenu =ref(false);
+</script>
+
+<style scoped>
+@media (max-width: 1023px) {
+  .navbar-menu {
+    position: absolute;
+    left: 0;
+    right: 0;
+  
+  }
+}
+</style>
